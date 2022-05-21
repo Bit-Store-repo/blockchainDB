c7a900e39800bfa58ab92899044e3b7a5918b7fd
code-simp <84436181+code-simp@users.noreply.github.com>
2022-05-21 16:30:32 +0530

    add: proc file

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3001 || process.env.PORT;
+const PORT = process.env.PORT || 3001;
 
 // requiring the blockchain functionalities
 const funcRouter = require('./src/functionalities/blockFunctions');
